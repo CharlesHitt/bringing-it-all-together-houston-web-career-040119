19093dc321746db54c43a9a4e793cab65ef12691
github username <github email address>
2019-04-10 07:49:44 -0500

    started over

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,101 +1,108 @@
 require 'pry'
 
-class Dog
-    attr_accessor :name, :breed, :id
-    def initialize(name:, breed:, id: nil)
-        @name = name
-        @breed = breed
-        @id = id
-      
-    end
+# class Dog
 
+#     attr_accessor :id, :name, :breed
+  
+#     def initialize(id: nil, name:, breed:)
+#       @id = id
+#       @name = name
+#       @breed = breed
+#     end
+  
+#     def self.create(hash)
+#       new_dog = Dog.new(name: hash[:name], breed: hash[:breed])
+#       new_dog.save
+#     end
+  
+#     def self.create_table
+#       DB[:conn].execute('CREATE TABLE dogs (id INTEGER PRIMARY KEY, name TEXT, breed TEXT)')
+#     end
+  
+#     def self.drop_table
+#       DB[:conn].execute('DROP TABLE dogs')
+#     end
+  
+#     def self.find_by_id(id)
+#       new_from_db(DB[:conn].execute('SELECT * FROM dogs WHERE id = ?', [id]).flatten)
+#     end
+  
+#     def self.new_from_db(data_array)
+#       Dog.new(id: data_array[0], name: data_array[1], breed: data_array[2])
+#     end
+  
+#     def self.find_or_create_by(name: ,breed: )
+#       if !DB[:conn].execute('SELECT * FROM dogs WHERE name = ? AND breed = ?', [name, breed]).flatten.empty?
+#         Dog.new_from_db(DB[:conn].execute('SELECT * FROM dogs WHERE name = ? AND breed = ?', [name, breed]).flatten)
+#       else
+#         Dog.create(name: name, breed: breed)
+#       end
+#     end
+  
+#     def self.find_by_name(name)
+#       new_from_db(DB[:conn].execute('SELECT * FROM dogs WHERE name = ?', [name]).flatten)
+#     end
+  
+#     def update
+#       DB[:conn].execute('UPDATE dogs SET name = ? WHERE id = ?', [self.name, self.id])
+#     end
+  
+#     def save
+#       DB[:conn].execute("INSERT INTO dogs (name, breed) VALUES (?, ?)", [self.name, self.breed])
+#       self.id = DB[:conn].execute('SELECT * FROM dogs WHERE name = ? AND breed = ?', [self.name, self.breed]).flatten[0]
+#       self
+#     end
+    ##restarting from scratch
+    class Dog
+
+    attr_accessor :id, :name, :breed
+  
+    def initialize(id: nil, name:, breed:)
+      @id = id
+      @name = name
+      @breed = breed
+    end
+  
+    def self.create(hash)
+      new_dog = Dog.new(name: hash[:name], breed: hash[:breed])
+      new_dog.save
+    end
+  
     def self.create_table
-        DB[:conn].execute('CREATE TABLE dogs(id INTEGER PRIMARY KEY, name TEXT, breed TEXT)')
+      DB[:conn].execute('CREATE TABLE dogs (id INTEGER PRIMARY KEY, name TEXT, breed TEXT)')
     end
-
+  
     def self.drop_table
-        DB[:conn].execute('DROP TABLE dogs')
+      DB[:conn].execute('DROP TABLE dogs')
     end
-
-    def self.create(name:, breed:)
-        dog = Dog.new(name: name,breed: breed)
-        dog.save
-        dog
-        # Dog.new(id: dog[0], name: dog[1], breed: dog[2])
-        # DB[:conn].execute('INSERT INTO dogs (name, breed) VALUES (?,?)',[name,breed])
-    end
-
-    # def find_by_id(id)
-    #     dog = DB[:conn].execute('SELECT * FROM dogs WHERE id = ?',[id]).flatten
-    #     dog_hash = {id:dog[0],name:dog[1],breed:dog[2]}
-    #     Dog.new(dog_hash)
-        
-    # end
-
-
-
-    def self.new_from_db(dog)
-    #    binding.pry
-        # DB[:conn].execute('INSERT INTO dogs (id,name,breed) VALUES (?,?,?)', [id: dog[0],name: dog[1], breed: dog[2]])
-        Dog.new(id: dog[0],name: dog[1], breed: dog[2])
-        # binding.pry
-    end
-
-    def self.find_by_name(name)
-        DB[:conn].execute('SELECT * FROM dogs WHERE name = ?',[name])
-    end
-
-    def update(name,breed)
-        self.name = name
-        self.breed = breed
-        DB[:conn].execute('UPDATE dogs SET name = ?, breed = ? WHERE id = ?', [name,breed,self.id])
-    end
-
-    def save
-        if self.id
-            self.update
-        else
-            DB[:conn].execute('INSERT INTO dogs(name,breed) VALUES(?,?)',[name,breed])
-        end
-        self
-    end
-
-
-    def self.find_by_name(name)
-        sql = <<-SQL
-          SELECT *
-          FROM dogs
-          WHERE name = ?
-          LIMIT 1
-        SQL
-    
-        DB[:conn].execute(sql,name).map do |row|
-          self.new_from_db(row)
-        end.first
-    end
-    
+  
     def self.find_by_id(id)
-        sql = <<-SQL
-          SELECT *
-          FROM dogs
-          WHERE id = ?
-          LIMIT 1
-        SQL
-    
-        DB[:conn].execute(sql,id).map do |row|
-          self.new_from_db(row)
-        end.first
+      new_from_db(DB[:conn].execute('SELECT * FROM dogs WHERE id = ?', [id]).flatten)
     end
-    
-   def self.find_or_create_by(name:,breed:)
-        if self.id
-            self.update
-        elsif self.new(id: dog[0],name: dog[1], breed: dog[2])
-        else DB[:conn].execute('SELECT * FROM dogs WHERE name,breed = (?,?)', [name,breed])
-        end
-   end
-       
-
-#   binding.pry
-#   0
+  
+    def self.new_from_db(data_array)
+      Dog.new(id: data_array[0], name: data_array[1], breed: data_array[2])
+    end
+  
+    def self.find_or_create_by(name: ,breed: )
+      if !DB[:conn].execute('SELECT * FROM dogs WHERE name = ? AND breed = ?', [name, breed]).flatten.empty?
+        Dog.new_from_db(DB[:conn].execute('SELECT * FROM dogs WHERE name = ? AND breed = ?', [name, breed]).flatten)
+      else
+        Dog.create(name: name, breed: breed)
+      end
+    end
+  
+    def self.find_by_name(name)
+      new_from_db(DB[:conn].execute('SELECT * FROM dogs WHERE name = ?', [name]).flatten)
+    end
+  
+    def update
+      DB[:conn].execute('UPDATE dogs SET name = ? WHERE id = ?', [self.name, self.id])
+    end
+  
+    def save
+      DB[:conn].execute("INSERT INTO dogs (name, breed) VALUES (?, ?)", [self.name, self.breed])
+      self.id = DB[:conn].execute('SELECT * FROM dogs WHERE name = ? AND breed = ?', [self.name, self.breed]).flatten[0]
+      self
+    end
 end
